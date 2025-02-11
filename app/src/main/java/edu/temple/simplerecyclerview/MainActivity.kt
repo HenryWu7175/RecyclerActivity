ca75a65d449e21eaac8b79fbc33d619da13fef56
HenryW7175 <143651504+HenryWu7175@users.noreply.github.com>
2025-02-11 12:15:02 -0500

    made recycler view without spinner and functions. made text view but does not do anything made funciton definitions for a custom adapter class

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,19 +2,34 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val numberArray = Array(50) { i -> (i + 1) * 5 }
+
 
         // TODO Step 1: Reference RecyclerView object
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val recyclerView =
+            findViewById<RecyclerView>(R.id.recyclerView)//this was made to combat the problem of relying on the developer to implement recycling
+        // recycling means that when you scroll, the textviews taht appear to you are the only views taht the computer keeps tracko f
+        // not recycling could lead to slower scrolling
+        val displayTextView =
+            findViewById<TextView>(R.id.textView)
 
         //TODO Step 2: Provide a LayoutManager
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
 
         //TODO Step 4: Provide a RecyclerView.Adapter
+        recyclerView.adapter = NumberDisplayAdapter(this@MainActivity, numberArray)
+
     }
 }
--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,14 +1,36 @@
 package edu.temple.simplerecyclerview
 
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* TODO Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter (private val _context : Context, private val _fonts : Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>()/* TODO Step 3a: Provide Adapter Parent */ {
+    //the thee adapter must be of VH type
 
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
+    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {
+        // TODO Step 3b: Complete function definitions for adapter
+    }
 
-    // TODO Step 3b: Complete function definitions for adapter
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        return NumberViewHolder(
+            TextView(
+                parent.context
+            ).apply{
+                setPadding(10,10,10,10)
+            }
+        )
+    }
 
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+       holder.textView.text = _fonts[position].toString()
+        holder.textView.textSize = _fonts[position].toFloat()
+    }
+
+    override fun getItemCount(): Int {
+        return _fonts.size
+    }
 }
